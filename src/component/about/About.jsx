ba533bab7f8c6ce7cd15b78053e1c59cfd631e31
ramonm85 <randsdata1@gmail.com>
2022-05-25 22:56:04 -0400

    about

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -26,22 +26,29 @@ const about = () => {
                         <article className='about_card'>
                         <FaAward className= 'about_icon'/>
                             <h5>Experince</h5>
-                            <small> 3+ years working</small>
+                            <small> 1 year working</small>
                         </article>
 
                         <article className='about_card'>
                         <FiUsers className= 'about_icon'/>
                             <h5>Clients</h5>
-                            <small> 200+ Worldwide</small>
+                            <small> 2 Worldwide</small>
                         </article>
                         <article className='about_card'>
                         <AiOutlineFolderOpen className= 'about_icon'/>
                             <h5>Projects</h5>
-                            <small> 80+ Completed</small>
+                            <small> 3+ Completed</small>
                         </article>
                     </div>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, perspiciatis veritatis qui voluptate, ea molestias rem et omnis praesentium incidunt magnam harum consequuntur ex, quas laborum dicta cumque assumenda! Cumque!
+                    Hello, my name is Ramon Marmol. I am the creator of ramonreacts.netlify.app. ( I Love to listen to music, spend time with friends and travel.
+                    I like to code and push myself learn new things,
+                    I would love to be a Web developer. 
+                    I created this website with the intention to show my ability to learn different javascript frameworks including "React". 
+                    with the inention to show that i am eager to expand my knoledge in this field. 
+                    Im hoping this allows you to cofide you in my ability to give you the confidence in the Knowledge ive thought myself  ) 
+                    Let me help you take your next step !!
+                    
                     </p>
                     <a href="#contact" className='btn btn-primary'> Lets Talk</a>
 
